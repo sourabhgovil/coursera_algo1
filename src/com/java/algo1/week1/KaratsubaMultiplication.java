d0301a8971be88ee34c6653d2fe74ed03d1228c6
sourabhgovil <sourabhgovil@gmail.com>
2017-05-15 23:00:16 -0700

    Initial commit

---
+++ src/com/java/algo1/week1/KaratsubaMultiplication.java
@@ -0,0 +1,28 @@
+package com.java.algo1.week1;
+
+import java.math.*;
+
+/**
+ * Created by sgovil on 5/13/17.
+ */
+public class KaratsubaMultiplication {
+    public static void main(String args[]) {
+
+        Karatsuba k = new Karatsuba();
+        System.out.println(k.NumLength(234599999999999999999));
+    }
+
+//    public int Karatsuba(BigInteger num1, BigInteger num2)
+//    {
+//
+//    }
+
+}
+
+class Karatsuba{
+    public BigInt NumLength(BigInteger number)
+    {
+
+        //return BigInteger.toString(number).length();
+    }
+}
